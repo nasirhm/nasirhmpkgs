7c8e10cf000715a5b41e45888c117729feb9aa40
nasirhm <nasirhussainm14@gmail.com>
2020-11-30 05:39:40 +0500

    :wrench: Addition of st in nasirhmpkgs
    Signed-off-by: nasirhm <nasirhussainm14@gmail.com>

--- default.nix
+++ default.nix
@@ -14,7 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
+  st = pkgs.callPackage ./pkgs/st { };
   # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
   # ...
 }
